04cea77e9f4c3ada2ca9123afd693cb0ccb20913
Arwa Mohammed <arwamohmd.32@gmail.com>
2024-10-10 17:11:44 +0300

    Update index.js

--- Frontend/e-learning/src/index.js
+++ Frontend/e-learning/src/index.js
@@ -1,28 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import reportWebVitals from './reportWebVitals';
-import Navbar from './Components/Navbar/navbar';
-import Home from './Components/home/home';
-import PopularCourses from './Components/PopularCourses/popularcourses';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import AllCourses from './Components/Courses/allCourses';
-import Login from './Components/Login/login';
-import Register from './Components/Register/register';
-import ViewProfile from './Components/ProfileManagment/ViewProfile';
-import CourseDetails from './Components/CourseDetails/coursedetails';
+import App from './Components/App';
+import { BrowserRouter as Router } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Navbar />
-    <Home />
-    <PopularCourses />
-    <AllCourses />
-    <Login />
-    <Register />
-    <CourseDetails/>
-    <ViewProfile />
-  </React.StrictMode>
+  <Router>
+    <App />
+  </Router> 
 );
-
-reportWebVitals();
