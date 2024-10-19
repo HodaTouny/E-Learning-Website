066b74306e28f6af1ebf437390f70ae03d1474a0
Arwa Mohammed Ramadan <arwamohmd.32@gmail.com>
2024-10-20 00:30:48 +0300

    ..

--- Frontend/e-learning/src/Components/App.js
+++ Frontend/e-learning/src/Components/App.js
@@ -4,7 +4,6 @@ import { UserProvider } from './userContext';
 import Navbar from './Navbar/navbar';
 import Home from './home/home';
 import AllCourses from './Courses/allCourses';
-import CourseDetails from './CourseDetails/coursedetails';
 import CourseDetail from './Courses/CourseDetail';
 import Login from './Login/login';
 import Register from './Register/register';
@@ -23,7 +22,6 @@ function App() {
                     <Route path="/" element={<Home />} />
                     <Route path="/about-us" element={<About />} />
                     <Route path="/courses" element={<AllCourses />} />
-                    <Route path="/CourseDetails" element={<CourseDetails />} />
                     <Route path="/login" element={<Login />} />
                     <Route path="/Register" element={<Register />} />
                     <Route path="/add-course-form" element={<UploadCourseForm />} />
