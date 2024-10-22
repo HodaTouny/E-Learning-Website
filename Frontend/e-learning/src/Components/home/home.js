64a815db5161ea86acf69125a8f4729cdb82ece5
Hoda Touny <100847847+HodaTouny@users.noreply.github.com>
2024-10-22 20:32:13 +0300

    remove un-needed data

--- Frontend/e-learning/src/Components/home/home.js
+++ Frontend/e-learning/src/Components/home/home.js
@@ -13,71 +13,6 @@ import PopularCourses from '../PopularCourses/popularcourses';
 import ContactUs from '../ContactUs/ContactUs'
 import Footer from '../Footer/Footer'
 
-const coursesData = [
-    {
-        id: 1,
-        categories: ['CSS', 'JavaScript'],
-        title: 'Quick Tip: Create a Very Simple Parallax Effect With CSS & JavaScript',
-        img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/parallax-pre.jpg',
-        link: 'https://webdesign.tutsplus.com/tutorials/quick-tip-how-to-build-a-dead-simple-parallax-effect-with-css-javascript--cms-33061?_ga=2.146015156.1249101154.1555312171-2126893531.1553152868',
-    },
-    {
-        id: 2,
-        categories: ['CSS', 'JavaScript'],
-        title: 'How to Build a Static Portfolio Page With CSS & JavaScript',
-        img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/chart-pre.png',
-        link: 'https://webdesign.tutsplus.com/tutorials/how-to-build-a-portfolio-page-with-css-javascript--cms-32933?_ga=2.268974834.267848227.1555345574-242901505.1471790948',
-    },
-    {
-        id: 3,
-        categories: ['CSS', 'JavaScript'],
-        title: 'How to Build a Responsive Multi-Column Layout With CSS Grid',
-        img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/parallax-pre.jpg',
-        link: 'https://webdesign.tutsplus.com/tutorials/how-to-build-a-responsive-multi-column-layout-with-css-grid--cms-32769',
-    },
-    {
-        id: 4,
-        categories: ['JavaScript', 'jQuery'],
-        title: 'How to Create a Lightbox with HTML, CSS, and jQuery',
-        img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/parallax-pre.jpg',
-        link: 'https://webdesign.tutsplus.com/tutorials/how-to-create-a-lightbox-with-html-css-and-jquery--cms-32847',
-    },
-    {
-        id: 5,
-        categories: ['JavaScript', 'WordPress'],
-        title: 'Creating a Responsive Slider with JavaScript and CSS',
-        img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/parallax-pre.jpg',
-        link: 'https://webdesign.tutsplus.com/tutorials/how-to-create-a-responsive-slider-with-javascript-and-css--cms-32945',
-    },
-    {
-        id: 6,
-        categories: ['CSS', 'JavaScript'],
-        title: 'Quick Tip: Using CSS Variables to Create a Color Theme',
-        img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/parallax-pre.jpg',
-        link: 'https://webdesign.tutsplus.com/tutorials/quick-tip-using-css-variables-to-create-a-color-theme--cms-32887',
-    },
-    {
-        id: 7,
-        categories: ['jQuery', 'WordPress'],
-        title: 'How to Create an Image Gallery with jQuery and CSS',
-        img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/parallax-pre.jpg',
-        link: 'https://webdesign.tutsplus.com/tutorials/how-to-create-an-image-gallery-with-jquery-and-css--cms-33035',
-    },
-    {
-        id: 8,
-        categories: ['CSS', 'JavaScript', 'fullPage.js'],
-        title: 'How to Create a Full-Screen Scrolling Website with fullPage.js',
-        img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/parallax-pre.jpg',
-        link: 'https://webdesign.tutsplus.com/tutorials/how-to-create-a-full-screen-scrolling-website-with-fullpagejs--cms-33108',
-    },
-    {
-        id: 9,
-        categories: ['CSS', 'JavaScript'],
-        title: 'Creating a CSS Card Layout with Flexbox',
-        img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/parallax-pre.jpg',
-        link: 'https://webdesign.tutsplus.com/tutorials/how-to-create-a-css-card-layout-with-flexbox--cms-32810',
-    },
-];
 
 function Home() {
     return (
@@ -104,7 +39,7 @@ function Home() {
                 </div>
             </section>
             <section id="PopularCourses" >
-                <PopularCourses courses={coursesData} />
+                <PopularCourses />
             </section>
             <section id="ContactUs">
                 <ContactUs />
